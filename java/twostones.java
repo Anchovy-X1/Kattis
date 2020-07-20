381f95ec4eb3f91494cc9d164899f1174db77a83
Anchovy-X1 <anchovy.x1@gmail.com>
2020-07-20 18:42:41 +0800

    upload java/twostones.java

---
+++ java/twostones.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class TwoStones {
+    private void run(){
+        Scanner s = new Scanner(System.in);
+        int num = Integer.parseInt(s.nextLine());
+        if (num%2==1) System.out.println("Alice");
+        else System.out.println("Bob");
+    }
+    public static void main(String[] args) {
+        TwoStones main = new TwoStones();
+        main.run();
+    }
+}
