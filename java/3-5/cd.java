3980eee187ef998ae84639b8e4c086e398447af6
Anchovy-X1 <anchovy.x1@gmail.com>
2020-07-28 23:23:25 +0800

    test

--- java/3-5/cd.java
+++ java/3-5/cd.java
@@ -11,17 +11,50 @@ public class CD {
         BufferedReader reader = new BufferedReader(inputStreamReader);
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
         BufferedWriter writer = new BufferedWriter(outputStreamWriter);
-        try {
-            ArrayList<Integer> arrayList = new ArrayList<>;
-            String[] str = reader.readLine().split(" ");
-            int N = Integer.parseInt(str[0]);
-            int M = Integer.parseInt(str[1]);
-            for(int i = 0; i < N; i++){
-                arrayList.add(Integer.parseInt(reader.readLine()));
-            }
-            int k = 0;
-            for(int i = 0; i < M; i++){
 
+        int N,M, ans,m,k;
+        int[] array;
+        String[] str;
+        try {
+            str = reader.readLine().split(" ");
+            N = Integer.parseInt(str[0]);
+            M = Integer.parseInt(str[1]);
+
+            if (N==0&&M==0) System.out.println(0);
+            while(!(N==0&&M==0)){
+                array = new int[N];
+                ans = 0;
+                for(int i = 0; i < N; i++){
+                    array[i] = Integer.parseInt(reader.readLine());
+                }
+
+                k = 0;
+                if (N>0){
+                    for (int i = 0;i<M;i++){
+                        m = Integer.parseInt(reader.readLine());
+                        while(array[k]<m){
+                            k++;
+                            if(k > N-1) break; //reach end of n
+                        }
+                        if(k > N-1){
+                            k = i;
+                            break;
+                        }
+                        if(array[k]==m){
+                            k++;
+                            ans++;
+                        }
+                    }
+                }
+                else k = -1;
+
+                for(int i = k+1; i<M; i++){
+                    reader.readLine();
+                }
+                writer.write(Integer.toString(ans)+'\n');
+                str = reader.readLine().split(" ");
+                N = Integer.parseInt(str[0]);
+                M = Integer.parseInt(str[1]);
             }
             writer.flush();
             reader.close();
--- java/3-5/teque.java
+++ java/3-5/teque.java
@@ -2,7 +2,69 @@
 https://open.kattis.com/problems/teque
 Difficulty: 3.5
 */
+import java.util.*;
+import java.io.*;
 
+public class Teque {
+
+    int front = 0;
+    int middle = 0;
+    int back = 0;
+    private void run(){
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
+        BufferedWriter writer = new BufferedWriter(outputStreamWriter);
+        try {
+            int N = Integer.parseInt(reader.readLine());
+            String[] str;
+            int num;
+            for(int i = 0; i < N; i++){
+                str = reader.readLine().split(" ");
+                num = Integer.parseInt(str[1]);
+                switch(str[0]){
+                    case "push_back":
+                        push_back(num);
+                        break;
+                    case "push_front":
+                        push_front(num);
+                        break;
+                    case "push_middle":
+                        push_middle(num);
+                        break;
+                    case "get":
+                        writer.write(get(num));
+                        break;
+                    default:
+                        throw new Exception("invalid method: " + str[0]);
+                }
+            }
+            writer.flush();
+            reader.close();
+            writer.close();
+        } catch(Exception e){
+            System.out.println("Error: "+e);
+        }
+    }
+    private void push_back(int x){
+        deque2.addLast(arrayList.size);
+        arrayList.add(x);
+    }
+    private void push_front(int x){
+        deque1.addLast(arrayList.size);
+        arrayList.add(x);
+    }
+    private void push_middle(int x){
+
+    }
+    private int get(int index){
+        return 1;
+    }
+    public static void main(String[] args) {
+        Teque main = new Teque();
+        main.run();
+    }
+}
 
 public class Node {
     private int value;
